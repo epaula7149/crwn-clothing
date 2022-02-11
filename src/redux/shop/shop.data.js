28224d738b27461d9c262eff5846efdf9b54c4e8
Elwyn Anderson <epaula7149@gmail.com>
2022-02-11 16:52:21 -0600

    moved directory and shop data into own reducer, created respective selectors and updated, updated components with new Redux flow for directory/shop

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -2,8 +2,8 @@
 
 /* Used as starting data for ShopPage */
 
-const SHOP_DATA = [
-  {
+const SHOP_DATA = {
+  hats: {
     id: 1,
     title: 'Hats',
     routeName: 'hats',
@@ -64,7 +64,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  sneakers: {
     id: 2,
     title: 'Sneakers',
     routeName: 'sneakers',
@@ -119,7 +119,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  jackets: {
     id: 3,
     title: 'Jackets',
     routeName: 'jackets',
@@ -156,7 +156,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  womens: {
     id: 4,
     title: 'Womens',
     routeName: 'womens',
@@ -205,7 +205,7 @@ const SHOP_DATA = [
       }
     ]
   },
-  {
+  mens: {
     id: 5,
     title: 'Mens',
     routeName: 'mens',
@@ -248,6 +248,6 @@ const SHOP_DATA = [
       }
     ]
   }
-];
+};
 
 export default SHOP_DATA;
